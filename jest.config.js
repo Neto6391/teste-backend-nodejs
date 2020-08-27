4fc8eaa3dcacd2784e36549559b86cf612eb0dc3
neto.rodrigues <neto6391@gmail.com>
2020-08-27 16:10:36 -0300

    chore: configure libs jest and eslint rules

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,20 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
