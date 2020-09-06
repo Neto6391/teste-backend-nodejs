b4ebb733daf712568f8c7670e883c9781c8b0868
neto.rodrigues <neto6391@gmail.com>
2020-09-06 16:33:40 -0300

    feat: ensure LogControllerDecorator calls controller with corrrect value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,14 @@
+import { HttpResponse, HttpRequest, Controller } from '../../presentation/controllers/signup/signup-protocols'
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.controller.handle(httpRequest)
+    return null
+  }
+}
