98b6bd2c1e98976d4953908221837cc9978a1f49
neto.rodrigues <neto6391@gmail.com>
2020-09-07 01:52:58 -0300

    test: add ocorrence

---
+++ src/presentation/controllers/ocorrence/ocorrence.spec.ts
@@ -0,0 +1,71 @@
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
+import { OcorrenceController } from './ocorrence'
+import { AddOcorrence, AddOcorrenceModel } from '../../../domain/usecases/add-ocorrence'
+import { OcorrenceModel } from '../../../domain/models/ocorrence'
+
+const makeAddOcorrence = (): AddOcorrence => {
+  class AddOcorrenceStub implements AddOcorrence {
+    async add (ocorrence: AddOcorrenceModel): Promise<OcorrenceModel> {
+      return new Promise(resolve => resolve(makeFakeOcorrence()))
+    }
+  }
+  return new AddOcorrenceStub()
+}
+
+const makeFakeOcorrence = (): OcorrenceModel => ({
+  id: 'any_id',
+  latitude: -9.648198,
+  longitude: -36.76422,
+  denunciante: {
+    nome: 'any_nome',
+    cpf: 'any_cpf'
+  },
+  denuncia: {
+    titulo: 'any_titulo',
+    descricao: 'any_descricao'
+  },
+  endereco: {
+    logradouro: 'any_logradouro',
+    bairro: 'any_bairro',
+    cidade: 'any_cidade',
+    estado: 'any_estado',
+    pais: 'any_pais',
+    cep: 'any_cep'
+  }
+})
+
+interface SubTypes {
+  sut: OcorrenceController
+  addOcorrenceStub: AddOcorrence
+}
+
+const makeSut = (): SubTypes => {
+  const addOcorrenceStub = makeAddOcorrence()
+  const sut = new OcorrenceController(addOcorrenceStub)
+  return {
+    sut,
+    addOcorrenceStub
+  }
+}
+
+describe('Ocorrence Controller', () => {
+  test('Should return 400 if no latitude is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        longitude: -36.76422,
+        denunciante: {
+          nome: 'any_nome',
+          cpf: 'any_cpf'
+        },
+        denuncia: {
+          titulo: 'any_titulo',
+          descricao: 'any_descricao'
+        }
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('latitude')))
+  })
+})
