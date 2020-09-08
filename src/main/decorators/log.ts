fe94b0daa1f49517386430a9f09298fe28adc555
neto.rodrigues <neto6391@gmail.com>
2020-09-08 02:52:29 -0300

    feat: add adapter axios for log decoration

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,18 +1,65 @@
 import { HttpResponse, HttpRequest, Controller } from '../../presentation/controllers/signup/signup-protocols'
 import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { AxiosPostGeocodingAdapter } from '../../infra/axios/axios-post-geocoding-adapter'
 
 export class LogControllerDecorator implements Controller {
   private readonly controller: Controller
   private readonly logErrorRepository: LogErrorRepository
+  private readonly axiosPostGeocodingAdapter: AxiosPostGeocodingAdapter
 
-  constructor (controller: Controller, logErrorRepository: LogErrorRepository) {
+  constructor (controller: Controller, logErrorRepository: LogErrorRepository, axiosPostGeocodingAdapter?: AxiosPostGeocodingAdapter) {
     this.controller = controller
     this.logErrorRepository = logErrorRepository
+    this.axiosPostGeocodingAdapter = axiosPostGeocodingAdapter
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!this.axiosPostGeocodingAdapter) {
+      const httpResponse = await this.controller.handle(httpRequest)
+      if (httpResponse.statusCode === 500) {
+        await this.logErrorRepository.logError(httpResponse.body.message.stack)
+      }
+      return httpResponse
+    } else {
+      const httResponseAxios = await this.axiosPostGeocodingAdapter.post(httpRequest)
+      if (httResponseAxios.statusCode === 500) {
+        await this.logErrorRepository.logError(httResponseAxios.body.message.stack)
+      }
+
+      const [{ locations = null }] = httResponseAxios.body
+      let endereco
+      if (!locations) {
+        endereco = {
+          logradouro: '',
+          bairro: '',
+          cidade: '',
+          estado: '',
+          pais: '',
+          cep: ''
+        }
+      } else {
+        const [{
+          street,
+          adminArea6,
+          adminArea5,
+          adminArea3,
+          adminArea1,
+          postalCode
+        }] = locations
+
+        endereco = {
+          logradouro: street,
+          bairro: adminArea6,
+          cidade: adminArea5,
+          estado: adminArea3,
+          pais: adminArea1,
+          cep: postalCode
+        }
+        httpRequest.body.endereco = endereco
+      }
+    }
     const httpResponse = await this.controller.handle(httpRequest)
-    if (httpResponse.statusCode === 500) {
+    if (httpResponse.statusCode === 500 || httpResponse.statusCode === 404) {
       await this.logErrorRepository.logError(httpResponse.body.message.stack)
     }
     return httpResponse
