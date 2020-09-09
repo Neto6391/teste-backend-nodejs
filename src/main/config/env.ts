e0f8715eda6e9ca9824ec9cbbad78bcde53fd366
neto.rodrigues <neto6391@gmail.com>
2020-09-08 22:06:48 -0300

    feat: add env variables for redis

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,7 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/teste-backend',
+  redisHost: process.env.REDIS_HOST || 'localhost',
+  redisPort: Number(process.env.REDIS_PORT) || 6379,
   port: process.env.PORT || 5050,
   keyGeocoding: process.env.KEY_GEOCODING || 'hLpA0KXVUTATD0vLAsL2BQOLPxJrhPmw'
 }
