02c8a3f1743e10b028292e4410d88e0be1209775
neto.rodrigues <neto6391@gmail.com>
2020-09-08 22:10:09 -0300

    feat: add cache in factory controller

--- src/main/factories/ocorrence.ts
+++ src/main/factories/ocorrence.ts
@@ -3,9 +3,11 @@ import { OcorrenceMongoRepository } from '../../infra/db/mongodb/ocorrence-repos
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../presentation/controllers/signup/signup-protocols'
 import { LogControllerDecorator } from '../decorators/log'
-import env from '../config/env'
 import { OcorrenceController } from '../../presentation/controllers/ocorrence/ocorrence'
 import { AxiosPostGeocodingAdapter } from '../../infra/axios/axios-post-geocoding-adapter'
+import { CacheControllerDecorator } from '../decorators/cache'
+import { AddressRedisRepository } from '../../infra/db/redis/address-repository/address-repository'
+import env from '../config/env'
 
 export const makeOcorrenceController = (): Controller => {
   const urlGeocoding = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${env.keyGeocoding}`
@@ -26,8 +28,9 @@ export const makeOcorrenceController = (): Controller => {
   const ocorrenceMongoRepository = new OcorrenceMongoRepository()
   const dbAddOcorrence = new DbAddOcorrence(ocorrenceMongoRepository)
   const axiosPostGeocodingAdapter = new AxiosPostGeocodingAdapter(urlGeocoding, bodyRequestAxios)
-
+  const addressRedisRepository = new AddressRedisRepository()
+  const cacheControllerDecorator = new CacheControllerDecorator(addressRedisRepository, axiosPostGeocodingAdapter)
   const ocorrenceController = new OcorrenceController(dbAddOcorrence)
   const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(ocorrenceController, logMongoRepository, axiosPostGeocodingAdapter)
+  return new LogControllerDecorator(ocorrenceController, logMongoRepository, cacheControllerDecorator)
 }
