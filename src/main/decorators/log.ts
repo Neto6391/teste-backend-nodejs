3d0ca5d58dbcc1a6fe98c4c479f25c75ad425844
neto.rodrigues <neto6391@gmail.com>
2020-09-08 22:09:04 -0300

    refactor: use dependency cache to redis

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,67 +1,40 @@
 import { HttpResponse, HttpRequest, Controller } from '../../presentation/controllers/signup/signup-protocols'
 import { LogErrorRepository } from '../../data/protocols/log-error-repository'
-import { AxiosPostGeocodingAdapter } from '../../infra/axios/axios-post-geocoding-adapter'
+import { CacheControllerDecorator } from './cache'
 
 export class LogControllerDecorator implements Controller {
   private readonly controller: Controller
   private readonly logErrorRepository: LogErrorRepository
-  private readonly axiosPostGeocodingAdapter: AxiosPostGeocodingAdapter
+  private readonly cachedData?: CacheControllerDecorator
 
-  constructor (controller: Controller, logErrorRepository: LogErrorRepository, axiosPostGeocodingAdapter?: AxiosPostGeocodingAdapter) {
+  constructor (controller: Controller, logErrorRepository: LogErrorRepository, cachedData?: CacheControllerDecorator) {
     this.controller = controller
     this.logErrorRepository = logErrorRepository
-    this.axiosPostGeocodingAdapter = axiosPostGeocodingAdapter
+    this.cachedData = cachedData
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!this.axiosPostGeocodingAdapter) {
-      const httpResponse = await this.controller.handle(httpRequest)
-      if (httpResponse.statusCode === 500) {
+    let httpResponse
+    if (this.cachedData) {
+      const httpResponseEndereco = await this.cachedData.handle(httpRequest)
+
+      if (httpResponseEndereco.statusCode === 500 || httpResponseEndereco.statusCode === 404) {
+        await this.logErrorRepository.logError(httpResponse.body.message.stack)
+      }
+      httpRequest.body.endereco = httpResponseEndereco.body
+
+      httpResponse = await this.controller.handle(httpRequest)
+
+      if (httpResponse.statusCode === 500 || httpResponse.statusCode === 404) {
         await this.logErrorRepository.logError(httpResponse.body.message.stack)
       }
       return httpResponse
     } else {
-      const httResponseAxios = await this.axiosPostGeocodingAdapter.post(httpRequest)
-      if (httResponseAxios.statusCode === 500) {
-        await this.logErrorRepository.logError(httResponseAxios.body.message.stack)
+      httpResponse = await this.controller.handle(httpRequest)
+      if (httpResponse.statusCode === 500 || httpResponse.statusCode === 404) {
+        await this.logErrorRepository.logError(httpResponse.body.message.stack)
       }
-
-      const [{ locations = null }] = httResponseAxios.body
-      let endereco
-      if (!locations) {
-        endereco = {
-          logradouro: '',
-          bairro: '',
-          cidade: '',
-          estado: '',
-          pais: '',
-          cep: ''
-        }
-      } else {
-        const [{
-          street,
-          adminArea6,
-          adminArea5,
-          adminArea3,
-          adminArea1,
-          postalCode
-        }] = locations
-
-        endereco = {
-          logradouro: street,
-          bairro: adminArea6,
-          cidade: adminArea5,
-          estado: adminArea3,
-          pais: adminArea1,
-          cep: postalCode
-        }
-        httpRequest.body.endereco = endereco
-      }
+      return httpResponse
     }
-    const httpResponse = await this.controller.handle(httpRequest)
-    if (httpResponse.statusCode === 500 || httpResponse.statusCode === 404) {
-      await this.logErrorRepository.logError(httpResponse.body.message.stack)
-    }
-    return httpResponse
   }
 }
